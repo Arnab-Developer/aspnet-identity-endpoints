f2db5743804d09996a6f4694581dac556aa71caa
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2023-11-19 12:47:50 +0530

    Fluent validation (#3)

---
+++ ApiAuth.WebApi/StudentValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiAuth.WebApi;
+
+internal class StudentValidator : AbstractValidator<Student>
+{
+    public StudentValidator()
+    {
+        RuleFor(s => s.FirstName).NotEmpty().WithMessage("Invalid first name");
+        RuleFor(s => s.LastName).NotEmpty().WithMessage("Invalid last name");
+    }
+}
