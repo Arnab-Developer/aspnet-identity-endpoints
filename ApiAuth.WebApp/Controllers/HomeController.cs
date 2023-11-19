3938345217dedac8685040e726e91bfb937d57cf
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2023-11-19 12:17:10 +0530

    Create student (#2)

--- ApiAuth.WebApp/Controllers/HomeController.cs
+++ ApiAuth.WebApp/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ public class HomeController(IHttpClientFactory factory, IOptionsMonitor<ApiSetti
     private readonly IHttpClientFactory _factory = factory;
     private readonly ApiSettings _settings = options.CurrentValue;
 
+    [HttpGet]
     public async Task<IActionResult> Index()
     {
         using var client = _factory.CreateClient();
@@ -21,6 +22,33 @@ public class HomeController(IHttpClientFactory factory, IOptionsMonitor<ApiSetti
         client.DefaultRequestHeaders.Add("Authorization", $"Bearer {loginResponse.AccessToken}");
         var students = await client.GetFromJsonAsync<IEnumerable<Student>>(_settings.WebApiUrl);
         return View(students);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        var student = new Student();
+        return View(student);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Student student)
+    {
+        if (!ModelState.IsValid) return View(student);
+
+        using var client = _factory.CreateClient();
+        var user = new User(_settings.Email, _settings.Password);
+        var response = await client.PostAsJsonAsync(_settings.WebApiLoginUrl, user);
+        response.EnsureSuccessStatusCode();
+
+        var loginResponse = await response.Content.ReadFromJsonAsync<LoginResponse>() ??
+            throw new InvalidOperationException();
+
+        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {loginResponse.AccessToken}");
+        var url = $"{_settings.WebApiUrl}?firstName={student.FirstName}&lastName={student.LastName}";
+        response = await client.PostAsync(url, default);
+        response.EnsureSuccessStatusCode();
+        return RedirectToAction("Index");
     }
 
     public IActionResult Privacy()
